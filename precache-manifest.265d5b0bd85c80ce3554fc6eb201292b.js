87626ca61ab3b672a55aa96db83cfe6e6aee2c8f
John Sasser <cw.john.sasser@gmail.com>
2020-05-05 13:33:08 -0400

    Updates

---
+++ precache-manifest.265d5b0bd85c80ce3554fc6eb201292b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9538002f988d04af6e564899e7d11533",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "d6085347ada6804c3b67",
+    "url": "/React-Portfolio/static/css/main.fea16a68.chunk.css"
+  },
+  {
+    "revision": "3b1065c3da40a799ad6b",
+    "url": "/React-Portfolio/static/js/2.fecd4f04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.fecd4f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6085347ada6804c3b67",
+    "url": "/React-Portfolio/static/js/main.b73f400b.chunk.js"
+  },
+  {
+    "revision": "910ea996016562bed44c",
+    "url": "/React-Portfolio/static/js/runtime-main.0af1ea3a.js"
+  },
+  {
+    "revision": "b32cf20d7badf713124cbc7672b5d270",
+    "url": "/React-Portfolio/static/media/express-js.b32cf20d.svg"
+  },
+  {
+    "revision": "c387e658dd4bb2aa2aacf04eddc272e5",
+    "url": "/React-Portfolio/static/media/filefish-min.c387e658.png"
+  },
+  {
+    "revision": "68e81ebda9c2b526d0e91d1ae1149c77",
+    "url": "/React-Portfolio/static/media/greenland-aurora-min.68e81ebd.jpg"
+  },
+  {
+    "revision": "167390c4c6ac755e191a47d6e2075672",
+    "url": "/React-Portfolio/static/media/john-headshots-5.167390c4.jpg"
+  },
+  {
+    "revision": "be0ff064a3ff57815259f00b8bdc3205",
+    "url": "/React-Portfolio/static/media/moab-road-min.be0ff064.jpg"
+  },
+  {
+    "revision": "cfceea7957779aa9c6c869185a0dedf5",
+    "url": "/React-Portfolio/static/media/mongodb-green.cfceea79.svg"
+  },
+  {
+    "revision": "94cafb0d1b4a2875215db17964f0938a",
+    "url": "/React-Portfolio/static/media/nodejs-green.94cafb0d.svg"
+  },
+  {
+    "revision": "e50428dfe9f70f0a8e5ffee304d28345",
+    "url": "/React-Portfolio/static/media/note-taker-min.e50428df.png"
+  },
+  {
+    "revision": "68304a98c70b3635d518df7afac940bf",
+    "url": "/React-Portfolio/static/media/property-hunter-min.68304a98.png"
+  },
+  {
+    "revision": "3ece720a0831e851fbe789681bfa1c55",
+    "url": "/React-Portfolio/static/media/react.3ece720a.svg"
+  },
+  {
+    "revision": "3ec85d6663943fd847f43968400f2a30",
+    "url": "/React-Portfolio/static/media/skyline-min.3ec85d66.jpg"
+  },
+  {
+    "revision": "063ce38c510fe6991a6d8cb906f916da",
+    "url": "/React-Portfolio/static/media/weather-dashboard-min.063ce38c.png"
+  }
+]);
